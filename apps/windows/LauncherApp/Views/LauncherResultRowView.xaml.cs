f7d9c9c96590b65b10c8584e71756e74c4311240
kunkka19xx <nobita079x@gmail.com>
2026-04-19 12:06:57 +0900

    comestic, refactor files

---
+++ apps/windows/LauncherApp/Views/LauncherResultRowView.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace LauncherApp.Views;
+
+public sealed partial class LauncherResultRowView : UserControl
+{
+    public LauncherResultRowView()
+    {
+        InitializeComponent();
+    }
+}
