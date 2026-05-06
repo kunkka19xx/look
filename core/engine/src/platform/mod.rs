651011ca5f9c2972be65cd07f878c93e9cbfd967
kunkka19xx <nobita079x@gmail.com>
2026-05-06 19:53:41 +0900

    lint

--- core/engine/src/platform/mod.rs
+++ core/engine/src/platform/mod.rs
@@ -1,16 +1,16 @@
 pub(crate) mod paths;
 
-#[cfg(target_os = "macos")]
-mod macos;
 #[cfg(target_os = "linux")]
 mod linux;
+#[cfg(target_os = "macos")]
+mod macos;
 #[cfg(target_os = "windows")]
 mod windows;
 
-#[cfg(target_os = "macos")]
-use macos as platform_impl;
 #[cfg(target_os = "linux")]
 use linux as platform_impl;
+#[cfg(target_os = "macos")]
+use macos as platform_impl;
 #[cfg(target_os = "windows")]
 use windows as platform_impl;
 
